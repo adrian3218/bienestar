d8819d9249e4d5f8afc44773615cf42b0a1fc41b
AdresNa7 <audy-188@hotmail.com>
2023-09-07 12:20:27 -0500

    se puso el logo y el color de fondo

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -4,35 +4,49 @@ import Button from '../components/Button';
 import Desplegable from '../components/Desplegable';
 import Fecha from '../components/Fecha';
 import Input from '../components/Input';
+import miImagen from './img/Logo.png'
 
 const Registro = () => {
   return (
     <div>
-      <h2>Registro</h2> 
-      <Input label="Nombre" />
-      <Input label="Apellidos" />
-      <Desplegable
+      <img src={miImagen} alt="Descripción de la imagen" />
+      <h1>REGISTRO<h6>No tienes una cuenta? Registrate aquí</h6></h1> 
+      <Input label="Nombres y Apellidos" />
+      <Desplegable  
         options={[
-          { value: 'dni', label: 'DNI' },
-          { value: 'cedula', label: 'Cédula' },
+          { value: 'Cosalud', label: 'Cosalud' },
+          { value: 'Salud total', label: 'Salud total' },
           { value: 'otro', label: 'Otro' },
         ]}
       />
-      <Input label="Número de Documento" />
-      <Input label="Teléfono" />
-      <Input label="Tipo de Sangre" />
-      <Input label="EPS" />
-      <Input label="Correo Institucional" />
-      <Input label="Correo Personal" />
-      <Input label="Contraseña" type="password" />
-      <Input label="Confirmación de Contraseña" type="password" />
-      <Desplegable
+       <Desplegable
         options={[
           { value: 'masculino', label: 'Masculino' },
           { value: 'femenino', label: 'Femenino' },
           { value: 'otro', label: 'Otro' },
         ]}
       />
+      <Desplegable
+        options={[
+          { value: 'Targeta de identidad', label: 'Targeta de identidad' },
+          { value: 'cedula', label: 'Cédula' },
+          { value: 'otro', label: 'Otro' },
+        ]}
+      />
+      <Input label="Número de Documento" />
+      <Input label="Teléfono" />
+      <Desplegable
+        options={[
+          { value: 'a+', label: 'A' },
+          { value: 'o+', label: 'O+' },
+          { value: 'b+', label: 'B+' },
+        ]} 
+      />
+      <Input label="Correo Institucional" />
+      <Input label="Correo Personal" />
+      <Input label="Contraseña" type="password" />
+      <Input label="Confirmación de Contraseña" type="password" />
+     
       <Fecha label="Fecha de Nacimiento" />
       <Desplegable
         options={[
@@ -44,9 +58,11 @@ const Registro = () => {
       <Input label="Número de Ficha" />
       <Input label="Dirección" />
       <Button label="Registrarse" />
+      <div className="politicas-privacidad">
+        <p>Al registrarte, aceptas nuestras <a href="#">Políticas de Privacidad</a>.</p>
+      </div>
     </div>
   );
 };
 
 export default Registro;
-
