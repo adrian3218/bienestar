6277d22a7e5ec29b6cc63365796929c4d1d5bc7e
AdresNa7 <audy-188@hotmail.com>
2023-09-11 08:20:01 -0500

    estilo del diseño completo

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,36 +1,43 @@
-import React from 'react';
+import React from "react";
 import Autocomplete from "../components/Autocomplete";
-import Button from '../components/Button';
-import Desplegable from '../components/Desplegable';
-import Fecha from '../components/Fecha';
-import Input from '../components/Input';
-import Politicas from '../components/Politicas';
+import Button from "../components/Button";
+import Desplegable from "../components/Desplegable";
+import Fecha from "../components/Fecha";
+import Input from "../components/Input";
+import Politicas from "../components/Politicas";
+import miImagen from "./img/Logo.png";
+import "./Registro.css";
 
 const Registro = () => {
   return (
     <div>
-
-      <h1>Registro<h6>No tienes una cuenta? Registrate aquí</h6></h1> 
+      <img src={miImagen} alt="Logo de bienestar" />
+      <h1>
+        REGISTRO
+        <h6>
+          <br></br>No tienes una cuenta? Registrate aquí
+        </h6>
+      </h1>
       <Input label="Nombres y Apellidos" />
-      <Desplegable  
+      <Desplegable
         options={[
-          { value: 'Cosalud', label: 'Cosalud' },
-          { value: 'Salud total', label: 'Salud total' },
-          { value: 'otro', label: 'Otro' },
-        ]}
-      />
-       <Desplegable
-        options={[
-          { value: 'masculino', label: 'Masculino' },
-          { value: 'femenino', label: 'Femenino' },
-          { value: 'otro', label: 'Otro' },
+          { value: "Cosalud", label: "Cosalud" },
+          { value: "Salud total", label: "Salud total" },
+          { value: "otro", label: "Otro" },
         ]}
       />
       <Desplegable
         options={[
-          { value: 'Targeta de identidad', label: 'Targeta de identidad' },
-          { value: 'cedula', label: 'Cédula' },
-          { value: 'otro', label: 'Otro' },
+          { value: "masculino", label: "Masculino" },
+          { value: "femenino", label: "Femenino" },
+          { value: "otro", label: "Otro" },
+        ]}
+      />
+      <Desplegable
+        options={[
+          { value: "Targeta de identidad", label: "Targeta de identidad" },
+          { value: "cedula", label: "Cédula" },
+          { value: "otro", label: "Otro" },
         ]}
       />
       <Input label="Correo Institucional" />
@@ -39,31 +46,34 @@ const Registro = () => {
       <Input label="Correo Personal" />
       <Desplegable
         options={[
-          { value: 'aprendiz', label: 'Aprendiz' },
-          { value: 'instructor', label: 'Instructor' },
-          { value: 'administrador', label: 'Administrador' },
-        ]} 
+          { value: "aprendiz", label: "Aprendiz" },
+          { value: "instructor", label: "Instructor" },
+          { value: "administrador", label: "Administrador" },
+        ]}
       />
       <Input label="Teléfono" />
       <Input label="Contraseña" type="password" />
-      <Autocomplete nombre= 'Ficha' array={[
-      { label: 2712267, programa: 'Programación de software'},
-      {label: 2812267, programa: 'Programación de software'},
-      {label: 2912267, programa: 'Programación de software' }
-      ]}/>
+      <Input label="Confirmación de Contraseña" type="password" />
+      <Autocomplete
+        nombre="Ficha"
+        array={[
+          { label: 2712267, programa: "Programación de software" },
+          { label: 2812267, programa: "Programación de software" },
+          { label: 2912267, programa: "Programación de software" },
+        ]}
+      />
       <Desplegable
         options={[
-          { value: 'a+', label: 'A' },
-          { value: 'o+', label: 'O+' },
-          { value: 'b+', label: 'B+' },
-        ]} 
+          { value: "a+", label: "A" },
+          { value: "o+", label: "O+" },
+          { value: "b+", label: "B+" },
+        ]}
       />
-      <Input label="Confirmación de Contraseña" type="password" />
       <Input label="Dirección" />
       <Button label="Registrarse" />
-      <Politicas label="Politicas"/>
+      <Politicas label="Politicas" />
     </div>
   );
 };
 
-export default Registro;
+export default Registro;
